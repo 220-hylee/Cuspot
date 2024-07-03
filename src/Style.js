d37a56a458f7923e77fbf7addfa0a64e716836de
ahn <ahnkong92@gmail.com>
2024-07-03 14:48:07 +0900

    완치

--- src/Style.js
+++ src/Style.js
@@ -89,4 +89,106 @@ export default makeStyles((theme) => ({
     width: "100%",
     height: "100%",
   },
+    //----gptResult추가
+    gpt_divtable : {
+      width : '100%',
+    },
+
+
+    gpt_root: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      padding: 20,
+      margin: 20,
+      height: '100vh',
+      backgroundColor: '#ffffff',
+      color: 'white',
+    },
+    gpt_paperContainer: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      width: '100%',
+      border: '1px solid transparent', // 투명한 경계선 설정
+      [theme.breakpoints.up('sm')]: {
+        width: '80%',
+      },
+      [theme.breakpoints.up('md')]: {
+        width: '60%',
+      },
+      [theme.breakpoints.up('lg')]: {
+        width: '50%',
+      },
+    },
+    gpt_table :{
+      marginTop: theme.spacing(5),
+      marginBottom: theme.spacing(10),
+      width: '100%',
+      borderCollapse: 'collapse',
+      border: "1px solid transparent",
+    },
+    gpt_th: {
+      padding: '12px 8px',
+      backgroundColor: '#1C1209',  // 배경색을 설정해주세요
+      color: 'white',
+      textAlign: 'center',
+      border: '1px solid transparent',
+      fontSize: '10px',
+    },
+    gpt_td: {
+      border: "1px solid transparent",
+      padding: '20px',
+      textAlign: 'justyfy-center',
+      fontSize: '15px',
+      fontWeight: 'bold',
+    },
+   gpt_tr: {
+      '&:nth-child(even)': {
+        border: "1px solid transparent",
+      },
+      '&:hover': {
+        backgroundColor: '#ddd',
+      },
+      fontSize: '20px',
+    },
+    gpt_logo: {
+      width: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 20,
+      "& > img": {
+        height: 35,
+      }
+    }, 
+    gpt_tablebody: {
+      fontSize: "20px",
+      fontWeight: "bold",
+      color: "black",
+      textAlign: "center",
+      padding: "20px",
+    },
+      // 뒤로가기 버튼
+      gpt_button: {
+        width: '100',  
+        height: 30,
+        border: "1px solid lightgrey",
+        display: "flex", // flex 속성 추가
+        flexDirection: "column",
+        borderRadius: 4,
+        color: "white",
+        // fontSize: 14,
+        // fontWeight: 600,
+        backgroundColor: 'black',
+        transition: 'all 0.5s ease', // 크기 변경 시 부드러운 애니메이션 추가
+          '@media (max-width: 768px)': { // 화면 너비가 768px 이하일 때
+            width: "30%",
+            fontSize: "1.2rem",
+          },
+          '@media (max-width: 480px)': { // 화면 너비가 480px 이하일 때
+            width: "40%",
+            fontSize: "1rem",
+    }
+        }
 }));
--- src/components/gptResult/GptResult.jsx
+++ src/components/gptResult/GptResult.jsx
@@ -1,127 +1,120 @@
-import React, { useEffect, useState } from "react";
-import {
-  Paper,
-  Typography,
-  CircularProgress,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from "@material-ui/core";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import { Paper, Typography, CircularProgress, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
+import axios from 'axios';
 import Header from "../header/Header";
-import Logo from "./../../assets/images/logo_width.png";
+import Logo from "../../assets/images/logo_width.png";
 import { Link } from "react-router-dom";
-import Style from "../../Style.js";
+import Style from "../../Style";
 import { Grid } from "@material-ui/core";
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
 
 const GptResult = () => {
+
+  // src/Style 사용
   const classes = Style();
 
   //useState를 사용해서, data, loading 상태를 관리
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  //useEffect를 사용해서, Google Sheets API를 호출하고 데이터를 가져옵
+  //Google Sheets API를 호출하고 데이터를 가져옴
   useEffect(() => {
-    const sheetId = "1iCCfFgbT43iVt0f_0cwUALr3i2-oLU8jn9brUc_3HMA";
-    const range = "Result Sheet!A:A"; // 구글 시트에서 가져 올 범위지정
-    const apiKey = "AIzaSyBP5KnUzW6BJaOgfeOjWA8RJhAiawg7Br0";
+    const sheetId = '1iCCfFgbT43iVt0f_0cwUALr3i2-oLU8jn9brUc_3HMA';
+    const range = 'Result Sheet!A:A'; // 구글 시트에서 가져 올 범위지정
+    const apiKey = 'AIzaSyBP5KnUzW6BJaOgfeOjWA8RJhAiawg7Br0';
 
     const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`;
-    axios
-      .get(url)
+    axios.get(url)
+
       // 데이터 가져오기 성공
-      .then((response) => {
+      .then(response => { 
         console.log(response.data.values); // 데이터를 콘솔에 출력하여 확인
         const rows = response.data.values;
-        if (rows.length > 0) {
-          setData([rows[rows.length - 1]]);
+        if(rows.length > 0){ 
+          setData([rows[rows.length-1]]); // 현재 행보다 -1 한걸 데이터에 담아줌
           //setData(rows);
         }
         setLoading(false);
       })
 
       //데이터 가져오기 실패
-      .catch((error) => {
-        console.error("Error fetching data: ", error);
+      .catch(error => {
+        console.error('Error fetching data: ', error);
         setLoading(false);
       });
   }, []);
+
+  //랜더링
   return (
-    <div className="GptResult">
-      <Grid className={classes.app}>
-        <Grid item container className={classes.app__header}>
-          <Header />
-        </Grid>
-        <Grid item container className={classes.body__friend} md={12}>
-          <Paper elevation={3} className={classes.root}>
-            <div className={classes.logo}>
-              <img
-                src={Logo}
-                style={{
-                  width: "300px",
-                  height: "100px",
-                  alignContent: "center",
-                }}
-                alt="linked-in-logo"
-              />
+
+    <div>
+    <Grid className={classes.app}>
+      <Grid item container className={classes.app__header}>
+        <Header />
+         <div className="GptResult">
+          <Paper className={classes.gpt_root} >
+            <div className={classes.gpt_logo}>
+                <img
+                  src={Logo}
+                  style={{ width: "300px", height: "100px", alignContent: "center" }}
+                  alt="linked-in-logo"
+                  />
             </div>
-            <Typography variant="h4" gutterBottom>
-              Custpot 운동 추천
-            </Typography>
-            <div id="data" className={classes.gptdata_paper}>
+            <div id="data" className={classes.gpt_paper}>
               {loading ? (
                 <div className={classes.loadingContainer}>
                   <CircularProgress />
                 </div>
-              ) : data.length > 0 ? (
-                <TableContainer
-                  component={Paper}
-                  className={classes.tableContainer}
-                >
-                  <div className={classes.divtable}>
-                    <Table className={classes.table}>
-                      <TableHead>
-                        <TableRow className={classes.tr}>
-                          <TableCell className={classes.th}>
-                            <h2>Result</h2>
-                          </TableCell>
+              ) : (
+                data.length > 0 ? (
+                  <TableContainer component={Paper} className={classes.gpt_paper}>
+                    <div className={classes.divtable}>
+                    <Table className={classes.gpt_table}>
+                        <TableRow className={classes.gpt_tr}>
+                          <TableCell className={classes.gpt_th}>
+                            <h2>  Custpot 운동 추천 </h2>
+                            </TableCell>
                         </TableRow>
-                      </TableHead>
-                      <TableBody className={classes.tablebody}>
+                      <TableBody className={classes.gpt_tablebody}>
                         {data.map((row, rowIndex) => (
-                          <TableRow key={rowIndex} className={classes.tr}>
+                          <TableRow key={rowIndex} className={classes.gpt_tr}>
                             {row.map((cell, cellIndex) => (
-                              <TableCell key={cellIndex} className={classes.td}>
-                                {cell}
-                              </TableCell>
+                              <TableCell key={cellIndex} className={classes.gpt_td}>{cell}</TableCell>
                             ))}
                           </TableRow>
                         ))}
                       </TableBody>
                     </Table>
                   </div>
-                </TableContainer>
-              ) : (
-                <Typography variant="body1" color="textSecondary">
-                  No data available
-                </Typography>
+                  </TableContainer>
+                ) : (
+                  <Typography variant="body1" color="textSecondary">
+                    No data available
+                  </Typography>
+                )
               )}
             </div>
-            <br />
-            <br />
-            <br />
-            <Link to="/App.js">
-              <button className={classes.button}>뒤로가기</button>
-            </Link>
+            <br /><br /><br />
+
+
+            {/* 버튼 이동-> 홈이랑, 맵 */}
+            <ButtonGroup variant="contained" color="primary">
+              <Link to="/App.js">
+                <Button className={classes.gpt_button}>HOME</Button>
+              </Link>
+              <Link to="/about">
+                <Button className={classes.gpt_button}>MAP</Button>
+              </Link>
+              </ButtonGroup>
+      
           </Paper>
-        </Grid>
-      </Grid>
     </div>
+      </Grid>
+    </Grid>
+  </div> 
   );
-};
+}
 
 export default GptResult;
