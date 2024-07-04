fb9dc227f1f01a4161530534b4cb31bcc526f6f8
hyenminkim <hyenmin8922@gmail.com>
2024-07-04 22:19:16 +0900

    채팅방 기능

--- src/components/chat/GroupChat.jsx
+++ src/components/chat/GroupChat.jsx
@@ -1,57 +1,35 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Style from "../../Style";
 import { Grid } from "@material-ui/core";
 import Header from "../header/Header";
 import { useSelector } from "react-redux";
+import { auth } from '../../firebase';
 
 const GroupChat = () => {
-  const { displayName, photoURL, email } = useSelector((state) => state.user);
-  // const iframeSrc = `http://localhost:3001/?displayName=${displayName}&photoURL=${photoURL}&email=${email}`;
-
-  
-
-   React.useEffect(() => {
-    fetch("http://localhost:3001/", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json;charset=utf-8",
-        },
-        body: JSON.stringify({
-            email: email,
-            username: displayName,
-            profile: photoURL,
-        }),
-    })
-    .then(response => {
-        console.log(`response`, response);   
-        if (response.status === 201) {
-            return response.json();     
-        } else {
-            return null;
-        }
-    })
-    .then(data => {
-        // 여기서 데이터 처리
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-}, [email, displayName, photoURL]);
-  
+  const { displayName, photoURL } = useSelector((state) => state.user);
   const classes = Style();
+  const [userId, setUserId] = useState(null);
+  const [user,setUser] = useState("");
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser({ userId: user.uid, displayName: user.displayName });
+      } else {
+        setUser({ userId: null, displayName: null });
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+  
   return (
-    
-    
-    
     <div>
       <Grid className={classes.app}>
         <Grid item container className={classes.app__header}>
           <Header />
-          <iframe src="https://chat-9eb84.web.app" width="100%" height="870px" title="chat"></iframe>
+          {/* iframe에 ref를 설정하고 src에 직접 메시지를 보내는 함수를 호출 */}
+          <iframe src={`https://chat-9eb84.web.app/?userId=${user.userId}&displayName=${displayName}`} width="100%" height="870px" title="chat"></iframe>
+          </Grid>
         </Grid>
-           
-      </Grid>
-      
     </div>
   );
 };
