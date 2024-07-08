5b56531fe25dd03695be25ad45f7c84f82c375a9
220-hylee <dooe207@naver.com>
2024-07-08 17:00:27 +0900

    hylee push

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import user from "./user";
 import util from "./util";
+import session from "./session";
 
 export default combineReducers({
   user,
   util,
+  session,
 });
